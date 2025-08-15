43a560270608ee2ba9b4b5deece26fa6d51e7a71
DamjanVincic <damjanvincic2003@gmail.com>
2025-08-15 12:49:50 +0200

    chore: generate django project (#1)

--- graph_visualizer/graph_visualizer/urls.py
+++ graph_visualizer/graph_visualizer/urls.py
@@ -1,12 +1,22 @@
+"""
+URL configuration for graph_visualizer project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
 from django.urls import path
 
-from . import views
-
 urlpatterns = [
-    path('', views.index, name="index"),
-    path("plugin/<str:plugin_identifier>/params/", views.get_plugin_params, name="get_plugin_params"),
-    path("load-data/", views.load_data, name="load_data"),
-    path("workspace/add/", views.add_workspace, name="add_workspace"),
-    path("workspace/set/", views.set_workspace, name="set_workspace"),
-    path("workspace/edit/", views.edit_workspace, name="edit_workspace"),
+    path('admin/', admin.site.urls),
 ]
