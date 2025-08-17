f782690682183a0db7a840f9f9dd597e6d9c6388
Mijat Krivokapić <mijat2004@gmail.com>
2025-08-17 18:47:25 +0200

    feat: implement load method (#5)

--- datasource_rdf/src/datasource_rdf/datasource.py
+++ datasource_rdf/src/datasource_rdf/datasource.py
@@ -1,6 +1,6 @@
 from api.model import Graph, Node, Edge
 from api.components.data_source_plugin import DataSourcePlugin
-from rdflib import Graph as RdfGraph
+from rdflib import Graph as RdfGraph,RDF
 
 
 class RdfDataSource(DataSourcePlugin):
@@ -11,5 +11,29 @@ class RdfDataSource(DataSourcePlugin):
         return "datasource_rdf"
 
     def load(self, **kwargs) -> Graph:
-        pass
+        file_path = kwargs['file_path']
+        rdf_graph = RdfGraph()
+        rdf_graph.parse(file_path, format='turtle')
+
+        nodes = {}
+        edges = []
+
+        for s, p, o in rdf_graph:
+            if s not in nodes:
+                    nodes[s] = {}
+            if o.__class__.__name__ == 'Literal':
+                nodes[s][p] = str(o)
+            elif p == RDF.type:
+                nodes[s]['type'] = str(o)
+            else:
+                edges.append((s, o))
+                if s not in nodes:
+                    nodes[s] = {}
+                if o not in nodes:
+                    nodes[o] = {}
+
+        graph_nodes = [Node(identifier, data) for identifier, data in nodes.items()]
+        graph_edges = [Edge(e[0], e[1]) for e in edges]
+
+        return Graph(graph_nodes, graph_edges, True)
 
