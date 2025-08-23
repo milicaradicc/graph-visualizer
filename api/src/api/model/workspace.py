089fa4bd23e674614df65f046900f969580df0e2
DamjanVincic <damjanvincic2003@gmail.com>
2025-08-23 15:17:33 +0200

    feat: implement backend graph filter (#23)

--- api/src/api/model/workspace.py
+++ api/src/api/model/workspace.py
@@ -1,9 +1,14 @@
-from . import Graph
-from typing import List, Tuple
+from typing import List, Union, TYPE_CHECKING
+from datetime import datetime
 
+from . import Graph
+
+
+if TYPE_CHECKING:
+    from . import Filter, Search
 
 class Workspace(object):
-    def __init__(self, id, graph: Graph, name: str, filters: List[Tuple[str, str, str]], searches: List[str]):
+    def __init__(self, id, graph: Graph, name: str, filters: List['Filter'], searches: List['Search']):
         self._graph = graph
         self._filters = filters
         self._searches = searches
@@ -16,7 +21,24 @@ class Workspace(object):
 
     @property
     def graph(self) -> Graph:
-        return self._graph
+        """
+        Returns a subgraph of the current graph filtered by the workspace's filters.
+        """
+        if not self._filters:
+            return self._graph
+
+        filtered_nodes = [
+            node for node in self._graph.nodes
+            if all(filter_obj(node) for filter_obj in self._filters)
+        ]
+        filtered_node_ids = {node.id for node in filtered_nodes}
+
+        filtered_edges = [
+            edge for edge in self._graph.edges
+            if edge.src.id in filtered_node_ids and edge.dest.id in filtered_node_ids
+        ]
+
+        return Graph(filtered_nodes, filtered_edges, self._graph.directed)
 
     @graph.setter
     def graph(self, value: Graph):
@@ -31,17 +53,24 @@ class Workspace(object):
         self._name = value
 
     @property
-    def filters(self) -> List[Tuple[str, str, str]]:
+    def filters(self) -> List['Filter']:
         return self._filters
 
     @filters.setter
-    def filters(self, value: List[Tuple[str, str, str]]):
+    def filters(self, value: List['Filter']):
         self._filters = value
 
     @property
-    def searches(self) -> List[str]:
+    def searches(self) -> List['Search']:
         return self._searches
 
     @searches.setter
-    def searches(self, value: List[str]):
+    def searches(self, value: List['Search']):
         self._searches = value
+
+    def add_filter(self, attribute: str, operator: str, value: Union[str, int, float, datetime]):
+        from . import Filter, FilterOperator
+        self._filters.append(Filter(attribute, FilterOperator(operator), value))
+
+    def remove_filter(self, filter_id):
+        self._filters = [f for f in self._filters if f.id != filter_id]
