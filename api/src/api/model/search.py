915c7bd8a6d3c7c87ba3cd5b8871c575cfaa3b2b
DamjanVincic <damjanvincic2003@gmail.com>
2025-08-23 20:32:22 +0200

    feat: add search and filter template (#23)

--- api/src/api/model/search.py
+++ api/src/api/model/search.py
@@ -3,14 +3,24 @@ from . import Node
 class Search:
     _id = 0
     def __init__(self, query: str):
-        self._id = Search._id
         Search._id += 1
+        self._id = Search._id
         self._query = query
 
     @property
     def id(self):
         return self._id
 
+    @property
+    def query(self) -> str:
+        return self._query
+
     def __call__(self, node: Node) -> bool:
         return self._query.lower() in [str(value).lower() for value in node.data.values()] \
                 or self._query.lower() in [str(key) for key in node.data.keys()]
+
+    def to_dict(self):
+        return {
+            "id": self._id,
+            "query": self._query
+        }
